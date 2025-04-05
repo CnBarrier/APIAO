5e3009723d50650a4dcfc059f42769aceef53954
CnBarrier <CnBarrier0921@outlook.com>
2025-04-05 20:58:54 +0800

    Add some features

--- Assets/Scripts/PlayerScripts.cs
+++ Assets/Scripts/PlayerScripts.cs
@@ -1,19 +1,26 @@
+using System.Threading;
 using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
-    [Header("人物")]
+    [Header("玩家")]
     public CharacterController controller;
     public Vector3 Velocity;
     public Vector3 targetPosition;
     public float Speed = 5f;
     private readonly float Gravity = -9.81f;
     private readonly float JumpHeight = 1f;
-    public bool isGrounded;
+    public int Health = 3;
+    public bool isGrounded = true;
+    public bool isAlive = true;
     
     [Space]
     [Header("道路判断")]
     [SerializeField] private int CurrentLand = 0; // 左0 中1 右2
     [SerializeField] private float LandSpace = 2f;
+
+    [Space]
+    [Header("计时器")]
+    public float timer = 60.0f;
  
     void Start()
     {
@@ -23,13 +30,15 @@ public class PlayerBehaviour : MonoBehaviour
     void Update()
     {
         PlayerMovement();
+        PlayerHealth();
+        CountDown();
     }
 
     private void PlayerMovement()
     {
         isGrounded = controller.isGrounded;
 
-        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        if(Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
             Velocity.y = Mathf.Sqrt(JumpHeight * Mathf.Abs(Gravity) * 2.0f);
         }
@@ -47,9 +56,50 @@ public class PlayerBehaviour : MonoBehaviour
         controller.Move(Speed * Time.deltaTime * (Direction + transform.forward)); 
         controller.Move(Velocity * Time.deltaTime);
 
-        if (!isGrounded)
+        if(!isGrounded)
         {
             Velocity.y += Gravity * Time.deltaTime;
         }
     }
+
+    private void PlayerHealth()
+    {
+        if(Health <= 0)
+        {
+            Dead();
+        }
+    }
+    
+    private void CountDown()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0f)
+        {
+            Dead();
+            return;
+        }
+    }
+
+    private void Dead()
+    {
+        isAlive = false;
+        Speed = 0.0f;
+        Debug.Log("Player is DEAD now");
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Money"))
+        {
+            Destroy(other.gameObject);
+        }
+        if(other.gameObject.CompareTag("Obstacles"))
+        {
+            Health -= 1;
+            Destroy(other.gameObject);
+        }
+    }
+
 }
